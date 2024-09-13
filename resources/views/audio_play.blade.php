37f868b329c3c3eebbc2b69d64b55b5233c51349
BHARATHVAJ111 <129038836+BHARATHVAJ111@users.noreply.github.com>
2024-09-13 11:00:53 +0530

    audio file change
    13-09-2024

--- resources/views/audio_play.blade.php
+++ resources/views/audio_play.blade.php
@@ -1,22 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-@if($audio->isEmpty())
-    <p>No Audio</p>
+@extends('layouts.layouts')
+
+        <table class="table w-50 container table table-bordered border-dark mt-3 table-success table-striped text-center">
+  <thead>
+    <tr class="">
+      <th scope="col">S.No</th>
+      <th scope="col">Audio</th>
+      
+    </tr>
+  </thead>
+  <tbody>
+  @if($audio->isEmpty())
+  <tr>
+    
+      <td colspan="2">No Audio Files Exist</td>
+    </tr>
 @else
-    @foreach($audio as $values)
-        <audio controls>
+    @foreach($audio as $key => $values)
+   
+    <tr>
+        <td class="">{{$key+1}}</td>
+      <td> <audio controls>
             <source src="{{ asset('storage/' . $values->file_path) }}" type="audio/mpeg">
             Your browser does not support the audio element.
-        </audio>
+        </audio></td>
+    </tr>
+  </tbody>
+</table>
     @endforeach
 @endif
-
-
-</body>
-</html>
